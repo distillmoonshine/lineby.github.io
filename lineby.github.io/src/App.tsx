679f91499e331718810d6560b0022b9b66696d39
Ganesh Pimpale <pimpale2003@gmail.com>
2022-11-01 15:28:25 -0700

    home

--- lineby.github.io/src/App.tsx
+++ lineby.github.io/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './pages/Home';
+import Create from './pages/Create';
+import About from './pages/About';
+import Tech from './pages/Tech';
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/create' element={<Create />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/tech' element={<Tech />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
